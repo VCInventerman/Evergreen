5580767081e2372865eee8317c03f6463b703c00
Nicholas Karr <nickkarr2005@gmail.com>
2020-08-03 17:35:16 -0500

    First commit

---
+++ EvergreenCompiler/src/EvergreenCompiler.cpp
@@ -0,0 +1,24 @@
+#include <map>
+#include <vector>
+#include <fstream>
+
+class CodeFile
+{
+	std::vector<std::string> lines;
+};
+
+std::string toType(const std::string& input)
+{
+
+}
+
+int main(int argc, char* argv[])
+{
+	std::cout << "Evergreen v1\n";
+
+	StockNameList names;
+	FillStockNameList(names, "ETF");
+
+
+	return EXIT_SUCCESS;
+}
